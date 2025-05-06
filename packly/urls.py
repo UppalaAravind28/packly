78715073af4bf5f90f1405df4190f312b04be0bb
Uppala Aravind <134129828+UppalaAravind28@users.noreply.github.com>
2025-05-06 15:14:36 +0530

    settings

--- packly/urls.py
+++ packly/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
     path('accounts/',include('accounts.urls'))
-]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
