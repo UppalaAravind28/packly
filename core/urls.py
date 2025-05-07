0725b5379eb0bf5fec353126a4ebd52c1f178fbe
UppalaAravind28 <aravind.uppala2002@gmail.com>
2025-05-07 14:42:15 +0530

    dependencies updated

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('index', views.index,name='index'),
     path('about', views.about, name='about'),
     path('news', views.news, name='news'),
     path('contact', views.contact, name='contact'),
